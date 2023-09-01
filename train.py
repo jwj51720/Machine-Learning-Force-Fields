7efd42aebe70c05bda3c5a112d1d1a3735bb57b1
Wonjun <jwj51720@gmail.com>
2023-09-02 00:17:09 +0900

    [Feat]: add train.py

---
+++ train.py
@@ -0,0 +1,26 @@
+import argparse
+import torch
+from utils.utils import set_seed, read_json
+
+
+def main(config):
+
+    pass
+
+
+if __name__ == "__main__":
+    args = argparse.ArgumentParser(description="samsung")
+    args.add_argument(
+        "-c",
+        "--config",
+        default="./config/base_config.json",
+        type=str,
+        help="config path",
+    )
+    args.add_argument("-t", "--task", default="force", type=str, help="force or energy")
+    args = args.parse_args()
+    config = read_json(args.config)
+
+    config["device"] = "cuda" if torch.cuda.is_available() else "cpu"
+    set_seed(config["seed"])
+    main(config)

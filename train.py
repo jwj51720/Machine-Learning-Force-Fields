97d4912b7804f36c6bc687cb24f7e312a05fdcf9
Wonjun <jwj51720@gmail.com>
2023-09-02 19:00:47 +0900

    [Fix]: debugging

--- train.py
+++ train.py
@@ -3,19 +3,26 @@ import torch
 from utils.utils import set_seed, read_json
 from dataset.dataset import ForceDataset, get_loader
 from dataset.preprocess import Preprocess
+from models.get_model import get_models
+from trainer.trainer import BaseTrainer
 
 
 def main(config):
+    print("=====================start preprocessing====================")
     preprocessor = Preprocess(config)
     train_data = preprocessor.load_train_data()
     train_df = preprocessor.preprocessing_force(train_data, is_train=True)
     train_df, valid_df = preprocessor.train_valid_split(train_df)
 
-    train_dataset = ForceDataset(train_df, is_train=True)
-    valid_dataset = ForceDataset(valid_df, is_train=False)
+    train_dataset = ForceDataset(train_df, config, is_train=True)
+    valid_dataset = ForceDataset(valid_df, config, is_train=True)
     train_loader, valid_loader = get_loader(train_dataset, valid_dataset, config)
-
-    pass
+    print(f'current device is {config["device"]}')
+    print("=======================start training=======================")
+    model = get_models(config)
+    trainer = BaseTrainer(model, train_loader, valid_loader, config)
+    trainer.training()
+    print("========================training done=======================")
 
 
 if __name__ == "__main__":
@@ -30,7 +37,7 @@ if __name__ == "__main__":
     args.add_argument("-t", "--task", default="force", type=str, help="force or energy")
     args = args.parse_args()
     config = read_json(args.config)
-
+    config["task"] = args.task
     config["device"] = "cuda" if torch.cuda.is_available() else "cpu"
     set_seed(config["seed"])
     main(config)
--- trainer/trainer.py
+++ trainer/trainer.py
@@ -1,5 +1,10 @@
 from torch.utils.data import DataLoader
 import torch.nn as nn
+import torch.optim as optim
+from torch.optim.lr_scheduler import ReduceLROnPlateau
+import os
+from tqdm import tqdm
+import torch
 
 
 class BaseTrainer:
@@ -10,4 +15,66 @@ class BaseTrainer:
         valid_loader: DataLoader,
         config: dict,
     ):
-        pass
+        self.model = model
+        self.train_loader = train_loader
+        self.valid_loader = valid_loader
+        self.config = config
+        self.cfg_trainer = self.config["trainer"]
+
+        self.criterion = nn.MSELoss()
+        self.optimizer = optim.Adam(
+            model.parameters(), lr=self.cfg_trainer["learning_rate"]
+        )
+        self.lr_scheduler = ReduceLROnPlateau(
+            self.optimizer,
+            patience=self.cfg_trainer["lr_scheduler"]["patience"],
+            factor=self.cfg_trainer["lr_scheduler"]["factor"],
+            mode=self.cfg_trainer["lr_scheduler"]["mode"],
+            verbose=True,
+        )
+
+        self.epochs = self.cfg_trainer["epochs"]
+        self.start_epoch = 1
+        self.save_dir = self.cfg_trainer["save_dir"]
+        if not os.path.exists(self.save_dir):
+            os.makedirs(self.save_dir)
+        self.min_val_loss = float("inf")
+        self.min_val_EF = float("inf")
+
+    def training(self):
+        for epoch in range(self.start_epoch, self.epochs + 1):
+            print(f"..{epoch} epoch..")
+            self.model.train()
+            for inputs, labels in tqdm(self.train_loader):
+                inputs = inputs.to(self.config["device"])
+                labels = labels.to(self.config["device"])
+                self.optimizer.zero_grad()
+                output = self.model(inputs)
+                loss = self.criterion(output, labels)
+                loss.backward()
+                self.optimizer.step()
+
+            val_loss, val_score = self.validation()
+            breakpoint()
+            print(
+                f"Train Loss: {loss}, Valid Loss: {val_loss}, Valid Score: {val_score}"
+            )
+            if val_loss < self.min_val_loss:
+                self.min_val_loss = val_loss
+                print("..min valid loss..")
+                print("..model save..")
+                torch.save(self.model.state_dict(), f"{self.save_dir}/current_best.pt")
+
+            if self.config["trainer"]["scheduler"]:
+                self.lr_scheduler.step(val_loss)
+
+    def validation(self):
+        val_loss = 0
+        self.model.eval()
+        with torch.no_grad():
+            for inputs, labels in tqdm(self.valid_loader):
+                output = self.model(inputs)
+                loss = self.criterion(output, labels)
+                val_loss += loss.item()
+                val_score = 1
+        return (val_loss / len(self.valid_loader), val_score)

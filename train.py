9bee21b6fd90c97a99a133879db4a359c31f30e0
Wonjun <jwj51720@gmail.com>
2023-09-02 15:14:03 +0900

    [Feat]: dataset, loader

--- train.py
+++ train.py
@@ -1,9 +1,19 @@
 import argparse
 import torch
 from utils.utils import set_seed, read_json
+from dataset.dataset import ForceDataset, get_loader
+from dataset.preprocess import Preprocess
 
 
 def main(config):
+    preprocessor = Preprocess(config)
+    train_data = preprocessor.load_train_data()
+    train_df = preprocessor.preprocessing_force(train_data, is_train=True)
+    train_df, valid_df = preprocessor.train_valid_split(train_df)
+
+    train_dataset = ForceDataset(train_df, is_train=True)
+    valid_dataset = ForceDataset(valid_df, is_train=False)
+    train_loader, valid_loader = get_loader(train_dataset, valid_dataset, config)
 
     pass
 
